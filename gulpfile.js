ce92e6bf68b0a8c0f9a6c7d4f6583d241ef7391b
Matt Clinton <matt@laralabs.uk>
2016-04-19 15:39:35 +0100

    Font Awesome and jQuery added. Gulpfile updated.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,20 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+
+    /* Main Bootstrap and Application Styles */
     mix.sass('app.scss');
+
+    /* Font Awesome CSS and Fonts */
+    mix.sass('font-awesome.scss');
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+
+    /* Glyphicons */
+    mix.copy('node_modules/bootstrap-sass/assets/fonts/bootstrap', 'public/fonts/bootstrap');
+
+    /* jQuery 2.2.0 */
+    mix.copy('node_modules/jquery/dist/jquery.js', 'public/js');
+
+    /* Bootstrap JS */
+    mix.copy('node_modules/bootstrap-sass/assets/javascripts/bootstrap.js', 'public/js');
 });
