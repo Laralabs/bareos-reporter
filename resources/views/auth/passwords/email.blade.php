a064c6883deadff0a54bdae94576b46c1611bf33
Matt Clinton <matt@laralabs.uk>
2016-04-20 11:56:45 +0100

    Page titles added to password reset views.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.hidden')
 
+@section('head')
+    <title>Bareos Reporter / Reset Password</title>
+@endsection
+
 <!-- Main Content -->
 @section('content')
 <div class="container">
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.hidden')
 
+@section('head')
+    <title>Bareos Reporter / Reset Password</title>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
