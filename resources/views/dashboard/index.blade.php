9b79a978a77c11db43f0922744d10b1b52924b44
Matt Clinton <matt@laralabs.uk>
2016-04-21 15:18:38 +0100

    Altered content section for new sidebar

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container content-container">
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
