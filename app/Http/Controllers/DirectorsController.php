f0003d880faf09708812e893edc99a89347bacac
Matt Clinton <matt@laralabs.uk>
2016-04-28 16:56:04 +0100

    Director Edit Save Complete.

--- app/Http/Controllers/DirectorsController.php
+++ app/Http/Controllers/DirectorsController.php
@@ -21,6 +21,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
 use Mockery\Exception;
 
 class DirectorsController extends Controller
@@ -247,7 +248,179 @@ class DirectorsController extends Controller
     public function save($id)
     {
         $director = Directors::find($id);
+        $catalog_id = $director->catalog_id;
+        $catalog = Catalogs::find($catalog_id);
 
-        return redirect('directors')->with('success', 'Director saved successfully.');
+        // Director Details
+        $director_name = Input::get('director_name');
+        $dir_ip_address = Input::get('director_ip');
+        $director_port = Input::get('director_port');
+
+        // Catalog Details
+        $catalog_name = Input::get('catalog_name');
+        $driver = Input::get('driver');
+        $engine = Input::get('engine');
+
+        if (isset($director_name)) {
+            $director->director_name = $director_name;
+        }
+        if (isset($dir_ip_address)) {
+            $director->ip_address = $dir_ip_address;
+        }
+        if (isset($director_port)) {
+            $director->director_port = $director_port;
+        }
+        if (isset($catalog_name)) {
+            $catalog->name = $catalog_name;
+        }
+        if (isset($driver)) {
+            $catalog->driver = $driver;
+        }
+        if (isset($engine)) {
+            $catalog->engine = $engine;
+        }
+
+        if ($driver == 'mysql') {
+
+            $host = Input::get('host-mysql');
+            $port = Input::get('port-mysql');
+            $database = Input::get('database-mysql');
+            $username = Input::get('username-mysql');
+            $password = Input::get('password-mysql');
+            if (isset($password)) {
+                $enc_password = Crypt::encrypt($password);
+            } else {
+                $enc_password = '';
+            }
+            $charset = Input::get('charset-mysql');
+            $collation = Input::get('collation-mysql');
+            $prefix = Input::get('prefix-mysql');
+            $strict = Input::get('strict-mysql');
+            $schema = Input::get('schema-pgsql');
+
+            if (isset($host)) {
+                $catalog->host = $host;
+            }
+            if (isset($port)) {
+                $catalog->port = $port;
+            }
+            if (isset($database)) {
+                $catalog->database = $database;
+            }
+            if (isset($username)) {
+                $catalog->username = $username;
+            }
+            if (isset($enc_password)) {
+                $catalog->password = $enc_password;
+            }
+            if (isset($charset)) {
+                $catalog->charset = $charset;
+            }
+            if (isset($collation)) {
+                $catalog->collation = $collation;
+            }
+            if (isset($prefix)) {
+                $catalog->prefix = $prefix;
+            }
+            if (isset($strict)) {
+                $catalog->strict = $strict;
+            }
+            if (isset($engine)) {
+                $catalog->engine = $engine;
+            }
+            if (isset($schema)) {
+                $catalog->schema = $schema;
+            }
+            else
+            {
+                $catalog->schema = '';
+            }
+            $catalog->save();
+
+            return redirect('directors')->with('success', 'Director updated successfully.');
+
+        } elseif ($driver == 'pgsql') {
+
+            $host = Input::get('host-pgsql');
+            $port = Input::get('port-pgsql');
+            $database = Input::get('database-pgsql');
+            $username = Input::get('username-pgsql');
+            $password = Input::get('password-pgsql');
+            if (isset($password)) {
+                $enc_password = Crypt::encrypt($password);
+            } else {
+                $enc_password = '';
+            }
+            $charset = Input::get('charset-pgsql');
+            $prefix = Input::get('prefix-pgsql');
+            $schema = Input::get('schema-pgsql');
+
+            if (isset($host)) {
+                $catalog->host = $host;
+            }
+            if (isset($port)) {
+                $catalog->port = $port;
+            }
+            if (isset($database)) {
+                $catalog->database = $database;
+            }
+            if (isset($username)) {
+                $catalog->username = $username;
+            }
+            if (isset($enc_password)) {
+                $catalog->password = $enc_password;
+            }
+            if (isset($charset)) {
+                $catalog->charset = $charset;
+            }
+
+            $catalog->collation = '';
+
+            if (isset($prefix)) {
+                $catalog->prefix = $prefix;
+            }
+
+            $catalog->strict = 0;
+
+            if (isset($engine)) {
+                $catalog->engine = $engine;
+            }
+            if (isset($schema))
+            {
+                $catalog->schema = $schema;
+            }
+            $catalog->save();
+
+            return redirect('directors')->with('success', 'Director updated successfully.');
+
+        } elseif ($driver == 'sqlite') {
+
+            $database = Input::get('database-sqlite');
+            $prefix = Input::get('prefix-sqlite');
+
+            $catalog->host = '';
+            $catalog->port = '';
+            $catalog->username = '';
+            $catalog->password = '';
+            $catalog->charset = '';
+            $catalog->collation = '';
+            $catalog->engine = null;
+            $catalog->schema = '';
+            $catalog->strict = 0;
+
+            if (isset($database)) {
+                $catalog->database = $database;
+            }
+            if (isset($prefix)) {
+                $catalog->prefix = $prefix;
+            }
+
+            $catalog->save();
+
+            return redirect('directors')->with('success', 'Director updated successfully.');
+
+        } else {
+            return Redirect::back()->with('error', 'Invalid driver selected');
+        }
     }
 }
