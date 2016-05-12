41e103e699eb28365d95090c64143aa1c0b5c90d
Matt Clinton <matt@laralabs.uk>
2016-05-12 16:15:20 +0100

    Better error checking if JobId is invalid

--- app/Console/Commands/RunJob.php
+++ app/Console/Commands/RunJob.php
@@ -58,100 +58,107 @@ class RunJob extends Command
     public function handle()
     {
         $jobId = $this->argument('job');
-        Log::info('Reporter:run artisan command');
-        if(!empty($jobId))
+        if(!empty($jobId) && $jobId != null)
         {
             $job = Jobs::find($jobId);
-            $schedule = Schedules::find($job->schedule_id);
-            $director = Directors::find($job->director_id);
-            $template = Templates::find($job->template_id);
-            $catalog = Catalogs::getDirectorCatalog($job->director_id);
-            $catalogName = $catalog->name;
-
-            $clients = json_decode($job->clients);
-            $contacts = json_decode($job->contacts);
-
-            $fileName = 'email'.$jobId.'.blade.php';
-            $code = $template->template_code;
-
-            $blade = fopen(resource_path('/views/email/'.$fileName), 'w');
-            if($blade !== false)
+            if($job != null)
             {
-                fwrite($blade, $code);
-                $tempBlade = fclose($blade);
+                $schedule = Schedules::find($job->schedule_id);
+                $director = Directors::find($job->director_id);
+                $template = Templates::find($job->template_id);
+                $catalog = Catalogs::getDirectorCatalog($job->director_id);
+                $catalogName = $catalog->name;
 
-                if($tempBlade === true)
+                $clients = json_decode($job->clients);
+                $contacts = json_decode($job->contacts);
+
+                $fileName = 'email'.$jobId.'.blade.php';
+                $code = $template->template_code;
+
+                $blade = fopen(resource_path('/views/email/'.$fileName), 'w');
+                if($blade !== false)
                 {
-                    // Let's build up the report data.
-                    $clientData = array();
-                    foreach($clients as $client)
-                    {
-                        $ukDate = date('Y-m-d');
-                        $usDate = date('Y-d-m');
-                        $datePattern = '';
-                        $ukCollection = DB::connection($catalogName)->table('Job')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
-                        if(count($ukCollection) > 0)
-                        {
-                            $datePattern = $ukDate;
-                        }
-                        else
-                        {
-                            $usCollection = DB::connection($catalogName)->table('Job')->where('StartTime', 'LIKE', '%'.$usDate.'%')->get();
+                    fwrite($blade, $code);
+                    $tempBlade = fclose($blade);
 
-                            if(count($usCollection) == 0)
+                    if($tempBlade === true)
+                    {
+                        // Let's build up the report data.
+                        $clientData = array();
+                        foreach($clients as $client)
+                        {
+                            $ukDate = date('Y-m-d');
+                            $usDate = date('Y-d-m');
+                            $datePattern = '';
+                            $ukCollection = DB::connection($catalogName)->table('Job')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
+                            if(count($ukCollection) > 0)
                             {
-                                Log::error('No US or UK Dates Found in Job Data');
-                                exit();
+                                $datePattern = $ukDate;
                             }
                             else
                             {
-                                $datePattern = $usDate;
+                                $usCollection = DB::connection($catalogName)->table('Job')->where('StartTime', 'LIKE', '%'.$usDate.'%')->get();
+
+                                if(count($usCollection) == 0)
+                                {
+                                    Log::error('No US or UK Dates Found in Job Data');
+                                    exit();
+                                }
+                                else
+                                {
+                                    $datePattern = $usDate;
+                                }
                             }
+
+                            //GATHER SUCCESS (T)
+                            $successCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'T')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
+                            $successCount = count($successCollection);
+
+                            //GATHER ERROR (f + E)
+                            $errorCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'f')->where('JobStatus', '=', 'E')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
+                            $errorCount = count($errorCollection);
+
+                            //GATHER WARNING (W)
+                            $warningCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'W')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
+                            $warningCount = count($warningCollection);
+
+                            $clientArray = array(
+                                'id'        =>  $client->id,
+                                'name'      =>  $client->name,
+                                'success'   =>  $successCount,
+                                'error'     =>  $errorCount,
+                                'warning'   =>  $warningCount
+                            );
+
+                            array_push($clientData, $clientArray);
                         }
 
-                        //GATHER SUCCESS (T)
-                        $successCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'T')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
-                        $successCount = count($successCollection);
-
-                        //GATHER ERROR (f + E)
-                        $errorCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'f')->where('JobStatus', '=', 'E')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
-                        $errorCount = count($errorCollection);
-
-                        //GATHER WARNING (W)
-                        $warningCollection = DB::connection($catalogName)->table('Job')->where('ClientId', '=', $client->id)->where('JobStatus', '=', 'W')->where('Type', '=', 'B')->where('StartTime', 'LIKE', '%'.$ukDate.'%')->get();
-                        $warningCount = count($warningCollection);
-
-                        $clientArray = array(
-                            'id'        =>  $client->id,
-                            'name'      =>  $client->name,
-                            'success'   =>  $successCount,
-                            'error'     =>  $errorCount,
-                            'warning'   =>  $warningCount
+                        $data = array(
+                            'clients'       =>  $clientData,
+                            'director'      =>  $director,
+                            'job'           =>  $job
                         );
+                        $email = Mail::send('email.email'.$jobId.'', $data, function ($message) use ($data) {
+                            $message->from('hello@reporter.com', 'Bareos Reporter');
 
-                        array_push($clientData, $clientArray);
+                            $job = $data['job'];
+                            $director = Directors::find($job->director_id);
+                            $contacts = json_decode($job->contacts);
+                            $message->subject(''.$director->director_name.' Backup Report: '.date('Y-m-d'));
+
+                            foreach($contacts as $contact)
+                            {
+                                $contactRecord = Contacts::find($contact);
+                                $message->to($contactRecord->email, $contactRecord->name);
+                            }
+                        });
+                        $deleteFile = unlink(resource_path('/views/email/'.$fileName));
                     }
-
-                    $data = array(
-                        'clients'       =>  $clientData,
-                        'director'      =>  $director,
-                        'job'           =>  $job
-                    );
-                    $email = Mail::send('email.email'.$jobId.'', $data, function ($message) use ($data) {
-                        $message->from('hello@reporter.com', 'Bareos Reporter');
-
-                        $job = $data['job'];
-                        $director = Directors::find($job->director_id);
-                        $contacts = json_decode($job->contacts);
-                        $message->subject(''.$director->director_name.' Backup Report: '.date('Y-m-d'));
-
-                        foreach($contacts as $contact)
-                        {
-                            $contactRecord = Contacts::find($contact);
-                            $message->to($contactRecord->email, $contactRecord->name);
-                        }
-                    });
                 }
+            }
+            else
+            {
+                Log::error('Job ID Invalid: '.$jobId);
             }
         }
     }
