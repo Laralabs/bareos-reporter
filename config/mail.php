4490267447ade83ed85562dffaa0f9ec7e5e2bf9
Matt Clinton <matt@laralabs.uk>
2016-05-18 10:58:41 +0100

    Put config/mail.php back to default

--- config/mail.php
+++ config/mail.php
@@ -11,8 +11,7 @@ return [
     | sending of e-mail. You may specify which one you're using throughout
     | your application here. By default, Laravel is setup for SMTP mail.
     |
-    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill",
-    |            "ses", "sparkpost", "log"
+    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "ses", "log"
     |
     */
 
@@ -55,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'matt@laralabs.uk', 'name' => 'Matt Clinton'],
+    'from' => ['address' => null, 'name' => null],
 
     /*
     |--------------------------------------------------------------------------
