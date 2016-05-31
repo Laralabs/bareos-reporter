134830ad1d92ff25cadd9d91fdb0b97513f907b5
Matt Clinton <matt@laralabs.uk>
2016-05-31 12:30:26 +0100

    Make Director Select box more prominent with border

--- gulpfile.js
+++ gulpfile.js
@@ -45,14 +45,14 @@ elixir(function(mix) {
     /* DataTables Tasks */
     elixir(function(mix) {
         mix.scripts([
-            '../../../node_modules/datatables.net/js/jquery.dataTables.js',
-            '../../../node_modules/datatables.net-bs/js/dataTables.bootstrap.js',
-            '../../../node_modules/datatables.net-autofill/js/dataTables.autoFill.js',
-            '../../../node_modules/datatables.net-autofill-bs/js/autoFill.bootstrap.js',
+            '../../../node_modules/datatables.net-buttons-bs/node_modules/datatables.net/js/jquery.dataTables.js',
+            '../../../node_modules/datatables.net-buttons-bs/node_modules/datatables.net-bs/js/dataTables.bootstrap.js',
             '../../../node_modules/datatables.net-buttons/js/dataTables.buttons.js',
             '../../../node_modules/datatables.net-buttons/js/buttons.colVis.js',
             '../../../node_modules/datatables.net-buttons/js/buttons.html5.js',
             '../../../node_modules/datatables.net-buttons-bs/js/buttons.bootstrap.js',
+            '../../../node_modules/datatables.net-autofill/js/dataTables.autoFill.js',
+            '../../../node_modules/datatables.net-autofill-bs/js/autoFill.bootstrap.js',
             '../../../node_modules/datatables.net-fixedheader/js/dataTables.fixedHeader.js',
             '../../../node_modules/datatables.net-responsive/js/dataTables.responsive.js',
             '../../../node_modules/datatables.net-responsive-bs/js/responsive.bootstrap.js'
@@ -61,9 +61,9 @@ elixir(function(mix) {
 
     elixir(function(mix) {
         mix.styles([
-            '../../../node_modules/datatables.net-bs/css/dataTables.bootstrap.css',
-            '../../../node_modules/datatables.net-autofill-bs/css/autoFill.bootstrap.css',
+            '../../../node_modules/datatables.net-buttons-bs/node_modules/datatables.net-bs/css/dataTables.bootstrap.css',
             '../../../node_modules/datatables.net-buttons-bs/css/buttons.bootstrap.css',
+            '../../../node_modules/datatables.net-autofill-bs/css/autoFill.bootstrap.css',
             '../../../node_modules/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.css',
             '../../../node_modules/datatables.net-responsive-bs/css/responsive.bootstrap.css'
         ], 'public/css/dataTables.css');
